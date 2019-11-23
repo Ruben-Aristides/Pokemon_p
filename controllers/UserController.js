803221a1dec54e3dea5618cc80c5c78016c00dc6
Ruben-Aristides <00181317@uca.edu.sv>
2019-11-23 10:54:37 -0600

    vista y Controles

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -25,16 +25,34 @@ userController.store = async function(req,res,next)
     user.evolucion=req.body.evolucion;
     user.habilidad1 = req.body.habilidad1;
     user.habilidad2 = req.body.habilidad2;
-    await user.save();
-    return res.status(200).json({"message":"Pokemon Agregado"});
+
+    try{
+        await user.save();
+        return res.status(200).json({"message":"Pokemon Agregado"});
+    }catch(err){
+        return res.status(500).json({ err: err, message: "Por favor revise sus datos" });
+    }
+    
 }
 
 //modificar usuario
-userController.update =async function(req,res,next)
-{
-    let {id} = req.params;
-    await User.update({_id:id},req.body);
-    res.status(200).json({"message":"Pokemon actualizado con exito"});
+userController.update = async function (req, res, next) {
+    let { id } = req.params;
+    let user = {
+        pokemon: req.body.pokemon,
+        tipo: req.body.tipo,
+        evolucion:req.body.evolucion,
+        habilidad1: req.body.habilidad1,
+        habilidad2: req.body.habilidad2
+    }
+    console.log(user);
+    try {
+        await User.update({ _id: id }, user);
+        res.status(200).json({ "message": "Pokemon actualizado con exito" });
+    }
+    catch (err) {
+        return res.status(500).json({ err: err, message: "Por favor revise sus datos" });
+    }
 }
 //eliminar usuario
 userController.delete = async function(req, res, next)
